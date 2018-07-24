e0480c251c909ca34affa45b4b2acf278c4c6459
Nathan  Visak <nathan.visak@gmail.com>
2018-07-24 17:26:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,4 +71,15 @@ class Dog
       self.create(name: row[1], breed: row[2])
     end
 
+    def self.find_by_name(name)
+      sql = <<-SQL 
+        SELECT * 
+        FROM dogs 
+        WHERE name = ? 
+      SQL
+
+      DB[:conn].execute(sql, name)
+    end
+      
+
 end
