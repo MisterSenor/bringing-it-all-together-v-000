5d6e2da7b8fa220e06ce14fc649db567ea795c3d
Nathan  Visak <nathan.visak@gmail.com>
2018-07-24 17:26:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,14 +72,15 @@ class Dog
     end
 
     def self.find_by_name(name)
-      sql = <<-SQL 
-        SELECT * 
-        FROM dogs 
-        WHERE name = ? 
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
       SQL
 
       DB[:conn].execute(sql, name)
+      binding.pry
     end
-      
+
 
 end
